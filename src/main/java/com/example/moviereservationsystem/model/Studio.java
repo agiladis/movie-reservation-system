fe55e770dd5aa44f436297629bdf55fe58b3c6eb
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-06 01:06:08 +0700

    [ADD] entity model and it relation

---
+++ src/main/java/com/example/moviereservationsystem/model/Studio.java
@@ -0,0 +1,30 @@
+package com.example.moviereservationsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "studios")
+public class Studio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @OneToMany(mappedBy = "studio", cascade = CascadeType.ALL)
+    private List<ShowTime> showTimes;
+}
---
+++ src/main/java/com/example/moviereservationsystem/model/Ticket.java
@@ -0,0 +1,34 @@
+package com.example.moviereservationsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "seat_number", nullable = false)
+    private String seatNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "showtime_id", nullable = false)
+    private ShowTime showTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "transaction_id", nullable = false)
+    private Transaction transaction;
+}
