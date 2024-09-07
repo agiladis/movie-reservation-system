5e5af4de2bd8b0dff0307c20fca79570c2f9945c
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-09-07 16:50:40 +0700

    [UPDATE] default value of role at user entity

--- src/main/java/com/example/moviereservationsystem/repository/UserRepository.java
+++ src/main/java/com/example/moviereservationsystem/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.example.moviereservationsystem.repository;
 
 import com.example.moviereservationsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
 }
